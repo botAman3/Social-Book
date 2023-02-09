ddd9502aabc3f4b4ea81fbe57bb50be2d64b68c7
botAman3 <kumaraman77420@gmail.com>
2023-02-09 16:26:55 +0530

    Created Profile Model

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.TextField((""))
-    id_user = models.TextField((""))
-    bio = models.TextField(("")) 
-    profileImg = models.ImageField(upload_to='profile_images')
-    location = models.TextField(("")) 
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True) 
+    profileImg = models.ImageField(
+        upload_to='profile_images' , default='blank-profile-picture.webp')
+    location = models.CharField(max_length=50 , blank=True) 
+
+    def __str__(self):
+        return self.user.username
